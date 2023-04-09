e6ab40204bf088ecb29c914b647343f4f2ca1f3b
shuga <124703939+ssmwoog@users.noreply.github.com>
2023-04-09 21:30:36 +0600

    Task 4 (finding “n!”)

---
+++ src/FactMachine.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FactMachine {
+    static int fact(int n){
+        if (n == 0) {
+            return 1;
+        } else {
+            return(n * fact(n - 1));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(fact(n));
+    }
+}
